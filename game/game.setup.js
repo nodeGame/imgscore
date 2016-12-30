8573c88a1cf02ddb98dbadf57640feaeb027ad6d
Stefano Balietti <futur.dorko@gmail.com>
2016-12-30 07:59:22 -0500

    mods

---
+++ game/game.setup.js
@@ -0,0 +1,39 @@
+/**
+ * # Game setup
+ * Copyright(c) 2016 Stefano Balietti <s.balietti@neu.edu>
+ * MIT Licensed
+ *
+ * The file includes settings for the nodeGame client instance
+ *
+ * This settings are then used to call `node.setup('nodegame')` and its
+ * remote version.
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+module.exports = function(settings, stages) {
+
+    var setup = {};
+
+    //auto: true = automatic run, auto: false = user input
+    setup.env = {
+        auto: false
+    };
+
+    setup.debug = true;
+
+    setup.verbosity = 1;
+
+    setup.window = {
+        promptOnleave: !setup.debug
+    };
+
+    // Metadata. Taken from package.json. Can be overwritten.
+    // setup.metadata = {
+    //    name: 'another name',
+    //    version: 'another version',
+    //    description: 'another descr'
+    // };
+
+    return setup;
+};
---
+++ game/game.stages.js
@@ -0,0 +1,18 @@
+/**
+ * # Game stages definition file
+ * Copyright(c) 2016 Stefano Balietti <s.balietti@neu.edu>
+ * MIT Licensed
+ *
+ * Stages are defined using the stager API
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+
+module.exports = function(stager, settings) {
+
+    // No stages.
+    // Stages are defined inside the client types.
+
+    return stager.getState();
+};
