e7fe12f6c5ff41ec00cd86d8fd07ac61f107d8a7
Stefano Balietti <futur.dorko@gmail.com>
2017-01-31 14:04:43 -0500

    requirements

--- auth/auth.settings.js
+++ auth/auth.settings.js
@@ -34,7 +34,7 @@ module.exports = {
      *               as parameters.
      *
      */
-    mode: 'auto',
+    mode: 'dummy',
 
     /**
      * ## nCodes
--- requirements/requirements.settings.js
+++ requirements/requirements.settings.js
@@ -10,7 +10,7 @@ module.exports = {
 
     // If enabled a Requirements room will be created in the channel,
     // and all incoming connections will be tested there.
-    enabled: false, // [false] Default: TRUE.
+    enabled: true, // [false] Default: TRUE.
 
     // Path the requirement room logic.
     // logicPath: './requirements.room.js',
@@ -19,6 +19,59 @@ module.exports = {
     // page: 'requirements.htm',
 
     // Optional. Maximum time to pass the tests of all requirements.
-    maxExecTime: 8000
+    maxExecTime: 8000,
 
+    /**
+     * ## viewportSize
+     *
+     * If set, client must have a resolution between the min and max specified
+     *
+     * Available options (more will be added):
+     *
+     *  - parser: (optional) a function that will parse the userAgent.
+     *            Default function is `ua-parser-js`
+     *  - cb: a callback that takes an object containing the parsed userAgent
+     *        and must return an object of the type:
+     *        { success: true/false, errors: undefined|array of strings }
+     *
+     * @see https://github.com/faisalman/ua-parser-js
+     */
+    browserDetect: {
+        cb: function(ua, params) {
+            if (ua.device.model || ua.device.type) {
+                return {
+                    success: false,
+                    errors: [ 'It seems you are using a mobile or tablet ' +
+                              'device. You can participate to this game ' +
+                              'only from a desktop or laptop computer with ' +
+                              'a keyboard and a mouse. If you can, try with ' +
+                              'another browser or device.' ]
+                };
+            }
+
+            return { success: true };
+        }
+    },
+
+    /**
+     * ## viewportSize
+     *
+     * Client must have a resolution between the min and max specified
+     */
+    viewportSize: {
+        minX: 800,
+        minY: 740
+    },
+
+    /**
+     * ## cookieSupport
+     *
+     * If set, client must support setting cookies.
+     *
+     * Accepted values:
+     *
+     *   - 'persistent': cookies must persist across session
+     *   - 'session': cookies must be set within same session
+     */
+    cookieSupport: 'persistent'
 };
