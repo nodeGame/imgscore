8d7075e4375f2defed4cc374b55ce6a2c3999679
Stefano Balietti <futur.dorko@gmail.com>
2017-05-04 17:51:09 -0400

    restrict sets

--- game/game.setup.js
+++ game/game.setup.js
@@ -12,7 +12,8 @@
 var NDDB = require('nodegame-client').NDDB;
 
 module.exports = function(settings, stages, gameDir, level) {
-    var setup = {};
+    var setup, tmp;
+    setup = {};
 
     // This settings will be made available to all client types.
     // It is up to them to use them.
@@ -52,6 +53,13 @@ module.exports = function(settings, stages, gameDir, level) {
 
     // Sets of 20 images (-extra contains info about incompatible sets).
     setup.sets = loadSets('sets-of-images-final-extra-MAY04.json');
+
+    // Optional. Restrict sets.
+    tmp = [ setup.sets[73], setup.sets[7], setup.sets[53],
+            setup.sets[56], setup.sets[72],
+            setup.sets[74], setup.sets[0] ]
+    setup.sets = tmp;
+
     // Sets of 40 images each.
     setup.randomSets = loadSets('sets-of-images-40.json');
     // All images paths, etc.
