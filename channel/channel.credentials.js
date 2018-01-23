438742d58b56482ac98d8b10fb101ad34329482c
Ewen <wang.yuxu@husky.neu.edu>
2018-01-22 22:03:52 -0500

    package.json info

--- channel/channel.credentials.js
+++ channel/channel.credentials.js
@@ -1,1 +1,18 @@
-../../../private/channel.credentials.js
+/**
+ * # Channel codes example file
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed
+ *
+ * Admin credentials.
+ *
+ * http://nodegame.org
+ * ---
+ */
+
+module.exports = function(settings, done) {
+ 
+    return {
+        user: 'admin',
+        pwd: 'admin'
+    };    
+};
--- channel/channel.secret.js
+++ channel/channel.secret.js
@@ -1,1 +1,11 @@
-../../../private/channel.secret.js
+/**
+ * # Channel secret for signing JWT tokens
+ * Copyright(c) 2015 Stefano Balietti
+ * MIT Licensed
+ *
+ * JWT secret file.
+ * ---
+ */
+module.exports = function(settings, done) {
+    return 'this is a secret';
+};
